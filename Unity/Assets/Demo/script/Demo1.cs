884d57704b76453e4a99ffb0b1c81bec90159db5
FanTang <gihhgihh11@gmail.com>
2019-07-31 10:15:23 +0800

    1. 增加 demo

--- Unity/Assets/Demo/script/Demo1.cs
+++ Unity/Assets/Demo/script/Demo1.cs
@@ -5,7 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript1 : MonoBehaviour
+public class Demo1 : MonoBehaviour
 {
     private const string agentServerIP = "http://35.187.146.144:30001"; //slither dev
     GamerEntity ai;
@@ -14,11 +14,22 @@ public class NewBehaviourScript1 : MonoBehaviour
     {
         ai = new GamerEntity(Guid.NewGuid().ToString());
         ai.Start(agentServerIP, new ConsoleBotProxy());
+        ai.onReceiveGamerInfo += Ai_onReceiveGamerInfo;
+    }
+
+    private void Ai_onReceiveGamerInfo(GamingRoom.Gaming.Packet.GamersPacket obj)
+    {
+        Debug.Log("Ai_onReceiveGamerInfo" + obj);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnApplicationQuit()
+    {
+        ai.Dispose();
     }
 }
