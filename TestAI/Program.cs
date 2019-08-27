deeeb99d27b235efe4f37cf7a291108be9ca7f20
frank12001 <gihhgihh11@gmail.com>
2019-08-27 19:35:44 +0800

    1. 刪除測試程式碼

--- TestAI/Program.cs
+++ TestAI/Program.cs
@@ -12,36 +12,9 @@ namespace TestAI
 {
     class Program
     {
-        const string userKey = "0452DA61-B201-49A4-B403-37B0760FFBCB";
-        //http://35.187.146.144:30001/   //35.229.204.195
-        private const string agentServerIP = "http://34.67.164.19:30001/"; //slither dev  http://192.168.2.5:30001
+        private const string agentServerIP = "http://34.80.230.34:30001/";
         static void Main(string[] args)
         {
-            //for (int i = 0; i < 1000; i++)
-            //{
-            //    Task.Run(() =>
-            //    {
-            //        var id = Guid.NewGuid().ToString();
-            //        Stopwatch stopWatch = Stopwatch.StartNew();
-            //        string res = Authentication("http://34.67.164.19:30003/", id);
-            //        stopWatch.Stop();
-            //        Console.WriteLine($" {id}, res: {res}, time: {stopWatch.ElapsedMilliseconds}");
-            //    });
-            //}
-            //Console.ReadLine();
-
-            //for (int i = 0; i < 1000; i++)
-            //{
-            //    Task.Run(() =>
-            //    {
-            //        Stopwatch stopWatch = Stopwatch.StartNew();
-            //        Urls url = QueryIP(agentServerIP);
-            //        stopWatch.Stop();
-            //        Console.WriteLine($" res: {url.UserUrl}, time: {stopWatch.ElapsedMilliseconds}");
-            //    });
-            //}
-            //Console.ReadLine();
-
             Console.WriteLine("Hello World!");
             List<GamerEntity> gamers = new List<GamerEntity>();
             for (int i = 0; i < 1000; i++)
@@ -58,37 +31,6 @@ namespace TestAI
                 ai.Start(agentServerIP, new ConsoleBotProxy());
                 return ai;
             }
-        }
-        public static string Authentication(string url, string devicedId)
-        {
-            var client = new RestClient(url);
-            var request = new RestRequest("Authentication");
-            var input = JsonConvert.SerializeObject(new { DeviceID = devicedId, SecretOpenID = "" });
-            input = StringEncrypt.Encrypt(input, userKey);
-            request.AddParameter("application/json", input, ParameterType.RequestBody);
-
-            var response = client.Post(request);
-            var content = StringEncrypt.Decrypt(response.Content, userKey);
-
-            var definition = new { Code = "", Msg = "", AccessToken = "" };
-            var result = JsonConvert.DeserializeAnonymousType(content, definition);
-
-            return result.AccessToken;
-        }
-        private static Urls QueryIP(string agentServerIP)
-        {
-            var input = JsonConvert.SerializeObject(new { ClientVersion = "1.9.1", LoginType = "Dev" });
-            input = StringEncrypt.Encrypt(input, userKey);
-
-            Uri agentServer = new Uri(agentServerIP);
-            var client = new RestClient(agentServer);
-            var request = new RestRequest("QueryIP");
-
-            request.AddParameter("Json", input, ParameterType.QueryString);
-            var response = client.Post(request);
-            var content = response.Content;
-
-            return JsonConvert.DeserializeObject<Urls>(content);
         }
     }
 }
